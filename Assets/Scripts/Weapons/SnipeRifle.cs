fc08c95bb8fd8f08770a3d4de5a9494a66235b1d
N1ck5tar <1032458@student.sae.edu.au>
2026-01-12 17:26:09 +1100

    added sniper rifle script, created scriptable object base stats as a weapon

---
+++ Assets/Scripts/Weapons/SnipeRifle.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SnipeRifle : WeaponBase
+{
+
+    // weapon - sniper rifle
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+}
