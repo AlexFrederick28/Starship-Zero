2c084a69d292086ed854d2bce63c4ec4fbadb746
N1ck5tar <1032458@student.sae.edu.au>
2026-02-24 21:24:02 +1100

    Added CARDS FUNCTIONAL item level on cards stats on item base for each item
    Fixed
    typo on crit damage card
    some functionality on cards

--- Assets/Scripts/LevelUp/LevelUpManager.cs
+++ Assets/Scripts/LevelUp/LevelUpManager.cs
@@ -2,14 +2,20 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using JetBrains.Annotations;
 public class LevelUpManager : MonoBehaviour
 {
     [SerializeField] public ItemManager itemManager;
 
+    // reference to the physical card
     [SerializeField] public GameObject CardGO1;
     [SerializeField] public GameObject CardGO2;
     [SerializeField] public GameObject CardGO3;
 
+    // reference to the 3 cards that can be chosen by the player
+    [SerializeField] public ItemCardInfo pickedCard1;
+    [SerializeField] public ItemCardInfo pickedCard2;
+    [SerializeField] public ItemCardInfo pickedCard3;
 
     [System.Serializable]
     public class ItemCardInfo
@@ -51,7 +57,7 @@ public class LevelUpManager : MonoBehaviour
 
     private void OnEnable()
     {
-        GameState.instance.OnPlayerLevelUp += LevelUpCards; 
+        GameState.instance.OnPlayerLevelUp += LevelUpCards;
     }
 
     private void OnDisable()
@@ -74,57 +80,172 @@ public class LevelUpManager : MonoBehaviour
     }
 
 
-    // NOTE - for item level text try "Level: itemLevelCount"
+
     public void RandomiseCards()
     {
         // setup list of cards to pick from
         cardListToChooseFrom = new List<ItemCardInfo>(cardList);
 
-        // Card 1
+        // reset cards chosen if needed
+        if (pickedCard1 != null && pickedCard2 != null && pickedCard3 != null)
+        {
+            pickedCard1 = null;
+            pickedCard2 = null;
+            pickedCard3 = null;
+        }
+
+        #region Card 1
         int randomCardIndex1 = Random.Range(0, cardListToChooseFrom.Count); // seperate int so it can be removed later
-        ItemCardInfo pickedCard1 = cardListToChooseFrom[randomCardIndex1]; // chosen 'card'
+        pickedCard1 = cardListToChooseFrom[randomCardIndex1]; // chosen 'card'
 
         //Debug.Log("Card 1 choice:" + pickedCard1.LevelCardSO.cardName);
 
         UIManager.instance.levelUpItemName[0].text = pickedCard1.LevelCardSO.cardName;
         UIManager.instance.levelUpItemStatDescription[0].text = pickedCard1.LevelCardSO.cardText;
-        UIManager.instance.levelUpItemLevel[0].text = pickedCard1.LevelCardSO.cardItemCount.ToString();
+
+        int itemLevel1 = CheckCardItemEffectType(pickedCard1);
+        UIManager.instance.levelUpItemLevel[0].text = "Level: " + itemLevel1.ToString();
+
         UIManager.instance.levelUpItemImage[0].sprite = pickedCard1.LevelCardSO.cardSprite;
 
         cardListToChooseFrom.RemoveAt(randomCardIndex1); // remove from temp pool of cards
+        #endregion
 
-
-        // Card 2
+        #region Card 2
         int randomCardIndex2 = Random.Range(0, cardListToChooseFrom.Count);
-        ItemCardInfo pickedCard2 = cardListToChooseFrom[randomCardIndex2];
+        pickedCard2 = cardListToChooseFrom[randomCardIndex2];
 
         //Debug.Log("Card 2 choice:" + pickedCard2.LevelCardSO.cardName);
 
         UIManager.instance.levelUpItemName[1].text = pickedCard2.LevelCardSO.cardName;
         UIManager.instance.levelUpItemStatDescription[1].text = pickedCard2.LevelCardSO.cardText;
-        UIManager.instance.levelUpItemLevel[1].text = pickedCard2.LevelCardSO.cardItemCount.ToString();
+
+        int itemLevel2 = CheckCardItemEffectType(pickedCard2);
+        UIManager.instance.levelUpItemLevel[1].text = "Level: " + itemLevel2.ToString();
+
         UIManager.instance.levelUpItemImage[1].sprite = pickedCard2.LevelCardSO.cardSprite;
 
         cardListToChooseFrom.RemoveAt(randomCardIndex2);
+        #endregion
 
-        // Card 3
+        #region Card 3
         int randomCardIndex3 = Random.Range(0, cardListToChooseFrom.Count);
-        ItemCardInfo pickedCard3 = cardListToChooseFrom[randomCardIndex3];
+        pickedCard3 = cardListToChooseFrom[randomCardIndex3];
 
         //Debug.Log("Card 3 choice:" + pickedCard3.LevelCardSO.cardName);
 
         UIManager.instance.levelUpItemName[2].text = pickedCard3.LevelCardSO.cardName;
         UIManager.instance.levelUpItemStatDescription[2].text = pickedCard3.LevelCardSO.cardText;
-        UIManager.instance.levelUpItemLevel[2].text = pickedCard3.LevelCardSO.cardItemCount.ToString();
+
+        int itemLevel3 = CheckCardItemEffectType(pickedCard3);
+        UIManager.instance.levelUpItemLevel[2].text = "Level: " + itemLevel3.ToString();
+
         UIManager.instance.levelUpItemImage[2].sprite = pickedCard3.LevelCardSO.cardSprite;
 
         cardListToChooseFrom.RemoveAt(randomCardIndex3);
-
+        #endregion
     }
 
-    public void CardSelected() // after the players picks a card
+    // card 1 2 3
+    public void CardSelected(int cardNumSelected) // after the players picks a card
     {
+        int itemLevelSelect = -1;
+
+        if (cardNumSelected == 1)
+        {
+            Debug.Log(pickedCard1.LevelCardSO.cardEffectType);
+            itemLevelSelect = EffectTypeCardCount(pickedCard1);
+        }
+
+        else if (cardNumSelected == 2)
+        {
+            itemLevelSelect = EffectTypeCardCount(pickedCard2);
+        }
+
+        else if (cardNumSelected == 3)
+        {
+            itemLevelSelect = EffectTypeCardCount(pickedCard3);
+        }
+
+        else
+        {
+            Debug.Log("CARD ERROR SELECTED: Num " + cardNumSelected);
+        }
+
+        if (itemLevelSelect != -1)
+        {
+
+            itemManager.itemCountGO[itemLevelSelect]++;
+            Debug.Log(itemManager.itemNamesGO[itemLevelSelect] + " level increased to: " + itemManager.itemCountGO[itemLevelSelect]);
+
+        }
+
+        itemManager.ItemScaleAllWeapons();
+
         GameState.instance.OnPlayerLevelUp?.Invoke(); // after card is done
     }
 
+    // all card item effect types should be checked
+    public int CheckCardItemEffectType(ItemCardInfo cardToCheck)
+    {
+        int itemManagerListValue = -1;
+
+        if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.AttackDamage)
+        {
+            // NUMBER BASED ON ITEM MANAGER LIST - ITEM LIST GO
+            itemManagerListValue = 0;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.AttackSpeed)
+        {
+            itemManagerListValue = 3;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.CritChance)
+        {
+            itemManagerListValue = 1;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.CritDamage)
+        {
+            itemManagerListValue = 2;
+        }
+
+        int itemCountValue = CheckItemManagerItemLevel(itemManagerListValue);
+
+
+        return itemCountValue;
+    }
+
+    public int CheckItemManagerItemLevel(int listValue)
+    {
+        int itemlevel = -1;
+
+        itemlevel = itemManager.itemCountGO[listValue];
+
+        return itemlevel;
+    }
+
+    public int EffectTypeCardCount(ItemCardInfo cardToCheck)
+    {
+        int itemManagerListValue = -1;
+
+        if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.AttackDamage)
+        {
+            // NUMBER BASED ON ITEM MANAGER LIST - ITEM LIST GO
+            itemManagerListValue = 0;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.AttackSpeed)
+        {
+            itemManagerListValue = 3;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.CritChance)
+        {
+            itemManagerListValue = 1;
+        }
+        else if (cardToCheck.LevelCardSO.cardEffectType == LevelCardScriptableObject.CardItemEffect.CritDamage)
+        {
+            itemManagerListValue = 2;
+        }
+
+        return itemManagerListValue;
+
+    }
 }
