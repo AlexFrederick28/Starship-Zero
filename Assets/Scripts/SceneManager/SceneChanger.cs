8f1f09dc3d0264fe51943d72e7feb8ed2a3d7387
N1ck5tar <1032458@student.sae.edu.au>
2026-03-12 17:09:21 +1100

    added Main menu - play, settings (no function), quit Scene changer

---
+++ Assets/Scripts/SceneManager/SceneChanger.cs
@@ -0,0 +1,55 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger instance;
+
+    private void OnEnable()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (instance != null)
+        {
+            instance = null;
+        }
+    }
+
+    public void LoadSceneByNumber(int sceneNum)
+    {
+        Debug.Log("Loading scene [" + sceneNum + "]");
+        SceneManager.LoadScene(sceneNum); // change scene
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        Debug.Log("Loading [" + sceneName + "] scene");
+        SceneManager.LoadScene(sceneName); // change scene
+    }
+
+    public void QuitTheGame()
+    {
+
+#if UNITY_EDITOR
+        Debug.Log("Quitting Editor");
+        EditorApplication.isPlaying = false;
+
+#if UNITY_STANDALONE_WIN
+        Debug.Log("Quitting Windows");
+        Application.Quit();
+#elif UNITY_WEBGL
+        Debug.Log("WebGL Build")
+#else
+        Debug.Log("Other");
+#endif
+#endif
+
+    }
+}
